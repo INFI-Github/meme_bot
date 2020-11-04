d2bccea3971c51c5762aca854ec8029c3d9efe3e
Jaishnu <73887147+Jaishnu@users.noreply.github.com>
2020-11-04 20:41:17 +0530

    Add files via upload

---
+++ MEMER_BOT.py
@@ -0,0 +1,29 @@
+import random
+
+
+import discord
+
+
+import praw as praw
+
+client = discord.Client()
+
+
+reddit = praw.Reddit(client_id='JQGiY5EzRhDwwg',
+                     client_secret='f4nFjO6XLJtHjt7MJ3eIRwxg9x463Q',
+                     user_agent='INFI_MEMES')
+
+
+@client.event
+async def on_message(message):
+    Channel_general = client.get_channel(773150585099911178)
+    if message.content == ".meme":
+        memes_submissions = reddit.subreddit('memes').hot()
+        post_to_pick = random.randint(1, 10)
+        for i in range(0, post_to_pick):
+            submission = next(x for x in memes_submissions if not x.stickied)
+
+        await Channel_general.send(submission.url)
+
+
+client.run("NzczMTQ5MDYxMjIxODQyOTU1.X6FBLA.cwaHyrXeeQNoIT8WrxtKNqTKijU")
